474a7ed92737991d15238a1f002c98bce8724bb9
alabi sodiq <adisco4420@gmail.com>
2019-03-10 00:31:50 +0100

    installing boostrap and header

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+        <header>
+            <h6>header of the page</h6>
+        </header>
+    );
+  }
+}
+
+export default Header;
