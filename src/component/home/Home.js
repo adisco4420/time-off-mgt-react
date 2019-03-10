19a256539c1d93e4a7611c9b77459dbf8fca28bd
alabi sodiq <adisco4420@gmail.com>
2019-03-10 01:15:12 +0100

    home component created

---
+++ src/component/home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Home extends Component { 
+    render() {
+        return (
+            <div>
+              <div class="jumbotron">
+                <h1 class="display-4">Time Off Management</h1>
+                <p class="lead">Open source, simple yet powerful absence management software for small and medium size business.</p>
+                <hr class="my-4" />
+                <p>Endorsed by hundreds of software developers..</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+                </p>
+            </div>
+        </div>
+        )
+    }
+
+}
+export default Home;
