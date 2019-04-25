95c7a2912d0d407c7dab56f22cdea85a096dbe0b
alabi sodiq <adisco4420@gmail.com>
2019-04-25 12:46:25 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path')
 const cors = require('cors');
 const EmployeeRoute = require('./routes/EmployeeRoute');
-
+const port = process.env.PORT || 6004; 
 const app = express();
 
 // Connect to MongoDB
@@ -24,21 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Use express static
-if (process.env.NODE_ENV !== "production") {
-    app.use(express.static(path.join(__dirname, '../public')));
-} else {
-    app.use(express.static(path.join(__dirname, '../build')));
-}
-app.get('/', (req, res) => {
-    if (process.env.NODE_ENV !== "production") {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    } else {
-        res.sendFile(path.join(__dirname, '../build/index.html'));
-    }
-});
+app.use(express.static(path.join(__dirname, '../build')));
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.use('/employee', EmployeeRoute);
 
-app.listen(6004).on('listening', () => {
-  console.log('We are live on ' + 6004);
+app.listen(port).on('listening', () => {
+  console.log('We are live on ' + port);
 });
