4e10308b2793e21d5c8c2a136b23b5f34e8058db
alabi sodiq <adisco4420@gmail.com>
2019-05-05 19:11:03 +0100

    make admin only access to team view

--- src/component/dashboard/team-view/TeamView.js
+++ src/component/dashboard/team-view/TeamView.js
@@ -2,45 +2,78 @@ import React , { Component } from 'react'
 // import { Link } from 'react-router-dom'
 import './style.css'
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 import Header from './../../header/Header'
 
-const allLeaveRequest = [
-    {   employee: 'Somoye', department: 'Accounting', DOR: '01/02/2018', 
-        leavedate: 'from 04/02/2018 to 10/02/2018', type: 'Vacation', days: 8 
-    },
-    {  employee: 'Tunde', department: 'Finance', DOR: '07/05/2018', 
-    leavedate: 'from 11/05/2018 to 20/05/2018', type: 'Medical Checkup', days: 9
-    },
-    {  employee: 'Mayowa', department: 'Marketing', DOR: '23/09/2018', 
-    leavedate: 'from 23/09/2018 to 30/05/2018', type: 'Special Occassion', days: 7
-    },
-]
-const allLeaves = [
-    {type: 'Vacation', deducted: '6', date: 'from 04/02/2018 to 10/02/2018', approvedBy:'Jide', status: 'Approved'},
-    {type: 'Medical Checkup', deducted: '9', date: 'from 11/05/2018 to 20/05/2018', approvedBy:'Thomas', status: 'Declined'}
-]
-
 class TeamView extends Component {
     state = {
-        allLeaveRequest: null
+        allLeaveRequest: null,
+        pendingLeave: null,
     }
     convertToshorDate = (date) => {
         return new Date(date).toLocaleDateString();
     }
+    getStatusColor(status) {
+        switch (status) {
+            case 'declined':
+                return 'badge-danger'
+            case 'approved': 
+                return 'badge-success'
+            default:
+                return 'badge-warning'
+        }
+    }
     componentDidMount() {
     const token = JSON.parse(localStorage.getItem('currentUserTimeOff')).token;
-    axios.get(`${process.env.REACT_APP_TimeOffURL}/leave`, 
+    axios.get(`${process.env.REACT_APP_TimeOffURL}/leave/all`, 
     {headers: { 'Authorization': `Bearer ${token}`}})
         .then((data) => {
-            console.log(data.data);
             const leaves = data.data.data
-            this.setState({allLeaveRequest: leaves})
+            this.getPendingLeaves(leaves)
+            this.setState({allLeaveRequest: leaves,})
         })
         .catch(err => {
             console.log(err.response);
         })
     }
+    confirmAction = (item, status) => {
+        let text = status === 'approved' ? 'approve' : 'decline'
+        Swal.fire({
+            title: 'Are you sure?',
+            html: `You want to <strong>${text}</strong>, <b>${item.name}</b> leave request`,
+            type: 'warning',
+            showCloseButton: true,
+        }).then((result) => {
+            if (result.va) {
+               this.updateLeave(item._id, status)
+            }
+        })
+    }
+
+    alertSuccess() {
+        Swal.fire({
+            title: 'Success',
+            html: `leave request updated`,
+            type: 'success',
+            showCloseButton: true,
+        })
+    }
+    async  updateLeave(id, status){
+        try {
+            const token = JSON.parse(localStorage.getItem('currentUserTimeOff')).token;
+            await axios.put(`${process.env.REACT_APP_TimeOffURL}/leave/${id}`, {status}, 
+                                            {headers: { 'Authorization': `Bearer ${token}`}})
+            this.alertSuccess()
+            this.componentDidMount()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getPendingLeaves(leaves) {
+        const leave =  leaves.filter(item => item.status === 'pending')
+        this.setState({pendingLeave: leave})
+    }   
     render() {
         return (
         <div>          
@@ -49,12 +82,12 @@ class TeamView extends Component {
             <h4>Admin Dashboard</h4>    
             <h6 className="text-info mb-2">Leave Request To Approve Or Decline</h6> 
             {
-                !this.state.allLeaveRequest ? 
+                !this.state.pendingLeave ? 
                 <p>Loading.....</p> : 
                 <div>
                     {
-                        !this.state.allLeaveRequest.length ? 
-                        <p>No Leave Request Available</p> : 
+                        !this.state.pendingLeave.length ? 
+                        <p>No Pending Leave Request Available</p> : 
             <table className="table table-hover">
               <thead>
                 <tr>
@@ -63,20 +96,21 @@ class TeamView extends Component {
                   <th>Date Of Request</th>
                   <th>Leave Date</th>
                   <th>Type</th>
+                  <th>More Actions</th>
                 </tr>
               </thead>
               <tbody>
                   {
-                      this.state.allLeaveRequest.map((item , index) => {
+                      this.state.pendingLeave.map((item , index) => {
                           return <tr key={index}>
                           <td>{item.name}</td>
                           <td>{item.department}</td>
                           <td>{this.convertToshorDate(item.date_created)}</td>
-                          <td>from {this.convertToshorDate(item.from_date)} to {this.convertToshorDate(item.to_date)}</td>
+                          <td>{this.convertToshorDate(item.from_date)} - {this.convertToshorDate(item.to_date)}</td>
                           <td>{item.leave_type}</td>
                           <td>
-                              <button className="btn btn-success btn-sm mr-2">Approve</button>
-                              <button className="btn btn-danger btn-sm">Decline</button>
+                              <button onClick={() => this.confirmAction(item, 'approved')} className="btn btn-success btn-sm mr-2">Approve</button>
+                              <button onClick={() => this.confirmAction(item, 'declined')} className="btn btn-danger btn-sm">Decline</button>
                           </td>
                         </tr>
                       })
@@ -90,36 +124,53 @@ class TeamView extends Component {
             }    
             
           <h6 className="text-info mt-5 mb-2"> All Leaves</h6>
-          <table className="table table-hover mb-5 px-4">
-              <thead>
-                <tr>
-                  <th>Type</th>
-                  <th>Deducted</th>
-                  <th>Date</th>
-                  <th>Approved By</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                  {
-                      allLeaves.map((item , index) => {
-                          return <tr key={index}>
-                          <td>{item.type}</td>
-                          <td>{item.deducted}</td>
-                          <td>{item.date}</td>
-                          <td>{item.approvedBy}</td>
-                          <td>{item.status}</td>
-
-                        </tr>
-                      })
-                  }
-
-              </tbody>
-            </table>
-          
+          <div className="col-md-12 text-center">
+                        {
+                            !this.state.allLeaveRequest ? <div>
+                                <h6>Loading <i className="fa fa-spinner"></i></h6>
+                            </div> : 
+                            <React.Fragment> 
+                                {
+                                    !this.state.allLeaveRequest.length ? 
+                                    <h6>No  Available Leave Request</h6> :
+                                    <table className="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th>Type</th>
+                                        <th>Employee</th>
+                                        <th>Date Requested</th>
+                                        <th>Leave Date</th>
+                                        <th>Approved By</th>
+                                        <th>Status</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                  
+                                        {
+                                            this.state.allLeaveRequest.map((item, index) => {
+                                                return <tr key={index}>
+                                                <td>{item.leave_type}</td>
+                                                <td>{item.name}</td>
+                                                <td>{this.convertToshorDate(item.date_created)}</td>
+                                                <td>{this.convertToshorDate(item.from_date)} - {this.convertToshorDate(item.to_date)}</td>
+                                                <td>{item.approved_by ? item.approved_by : '--'}</td>
+                                                <td> <span className={`badge ${this.getStatusColor(item.status)} text-light bgd`}>{item.status}</span> </td>
+                                            </tr>
+                                            })
+                                        }
+                           
+                                    </tbody>
+                            </table>
+                            }
+                           </React.Fragment>
+                        }
+                
+                        </div>
+                    </div>
+               
           
           </div>
-          </div>
+  
  
           
         )
